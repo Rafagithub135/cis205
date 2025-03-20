e6b2806b1111c69a50e08654852fa648d290dc8c
Ralph M. <mccloskey.ralph35@gmail.com>
2025-03-20 06:25:17 -0400

    working on multi table select statements

--- Pizza_Delivery/enterMetaData.sql
+++ Pizza_Delivery/enterMetaData.sql
@@ -15,18 +15,15 @@ INSERT INTO customer (custFName, custLName, custPhone)
 
 INSERT INTO deliveryPerson (delPhone, delModel, delName)
     VALUES
-        (2158348599, 'Escort', 'Fred Sanford');
-
-INSERT INTO deliveryPerson (delPhone, delModel, delName)
-    VALUES
+        (2158348599, 'Escort', 'Fred Sanford'),
         (2157354677, 'Ranger','Roger Rabbit'),
         (123456789, 'Escalade','George Winston');
 
 -- On the many side, values must have values for foreign keys in one table.  If orderNum is set for auto increment, leave data blank when entering.
 INSERT INTO orders (orderNum, orderDate, orderTime, empNum_FK, delPhone_FK, custPhone_FK)
     VALUES
-    ('01', 2025/01/15, 1600, 'T100', 2158348599, 7137552311),
-    ('02', 2025/01/17, 1035, 'T101', 2158348599, 7178991234);
+    ('01', 2025-01-15, 1600, 'T100', 2158348599, 7137552311),
+    ('02', 2025-01-17, 1035, 'T101', 2158348599, 7178991234);
 
 INSERT INTO pizza (pSize, pPrice)
     VALUES
