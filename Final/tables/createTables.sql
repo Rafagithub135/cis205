d1cc1ef276c432c84636538fe4ed1d7510e25ada
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2025-03-26 11:38:37 -0400

    created final project file

---
+++ Final/tables/createTables.sql
@@ -0,0 +1,11 @@
+USE dealership;
+
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS vehicles;
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE locations
+(
+    locID
+);
